b1f23c9077f78967ad6753a904634706311fc317
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-07-25 20:43:13 +0900

    감정 선택 페이지 기초 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import SignUp from "./pages/SignUp";
 import Calender from "./pages/Calender";
+import Emotion from "./pages/Emotion";
 
 const App = () => {
-  return <Calender />;
+  return <Emotion />;
 };
 
 export default App;
--- src/pages/Emotion.jsx
+++ src/pages/Emotion.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import backgroundImage from "../assets/background.png";
 
 const Emotion = () => {
-  return <div>Emotion</div>;
+  return (
+    <div
+      className="w-full h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="bg-white w-[90%] h-[80%] rounded-2xl shadow-2xl p-22 relative">
+        <h1 className="text-6xl font-bold text-gray-500 text-left">
+          오늘의 감정은?
+        </h1>
+        <hr className="my-8 text-gray-500 border-t-2" />
+      </div>
+    </div>
+  );
 };
 
 export default Emotion;
