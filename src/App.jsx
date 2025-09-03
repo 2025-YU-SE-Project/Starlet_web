40fc133fe918508cf19dbe6af6da17b28129138e
chominseo <singer100@yu.ac.kr>
2025-09-03 13:53:45 +0900

    chore: 폰트 적용 및 App.jsx 라우팅 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SignIn from './pages/SignIn'
-import Mypage from './pages/Mypage'
+
+
+
 
 const App = () => {
   return (
      <BrowserRouter>
       <div className="bg-cover min-h-screen bg-[url('/src/assets/background.png')]">
         <Routes>
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/mypage' element={<Mypage />} />
-
+       
         </Routes>
       </div>
     </BrowserRouter>
