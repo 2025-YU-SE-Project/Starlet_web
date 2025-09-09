cdd3178d1ab351b7ef0fc35d9a933af31af1279f
chominseo <singer100@yu.ac.kr>
2025-09-09 15:40:13 +0900

    ref: App.jsx AuthProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from './pages/Signup'
 import SignIn from './pages/SignIn'
-
+import AuthProvider from "./contexts/AuthProvider";
 
 
 
 const App = () => {
   return (
+    <AuthProvider>
      <BrowserRouter>
       <div className="bg-cover min-h-screen bg-[url('/src/assets/background.png')]">
+   
         <Routes>
           <Route path='signup' element={<Signup/>}/>
           <Route path='signin' element={<SignIn/>}/>
         </Routes>
       </div>
     </BrowserRouter>
+    </AuthProvider>
 
   )
 }
